2cb62d567e8a2727abd944fda31abc707b657b3e
Peter Buckley <8352591+pbuckley@users.noreply.github.com>
2025-04-08 15:43:23 -0400

    create annotation with a build success report

--- .buildkite/scripts/build_deployable_service.sh
+++ .buildkite/scripts/build_deployable_service.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-echo "Building ${BUILDKITE_PIPELINE_NAME} with version $(buildkite-agent meta-data get rel-ver) for deploy to hosts $(buildkite-agent meta-data get deploy-hosts)..."
+export REL_VER=$(buildkite-agent meta-data get rel-ver)
+export DEPLOY_HOSTS=$(buildkite-agent meta-data get deploy-hosts)
+
+echo "Building ${BUILDKITE_PIPELINE_NAME} with version ${REL_VER} for deploy to hosts ${DEPLOY_HOSTS}..."
+
+# we are assuming success here, this is a mock build that does not demo failure
+
+IFS=',' read -ra DEPLOY_HOST_ARRAY <<< "$DEPLOY_HOSTS"
+
+cat > "build-success.md" <<EOF
+# 📊 Build Success Report
+
+## Build Information
+
+** Application:** \`${BUILDKITE_PIPELINE_NAME}\`
+** Version:** \`${REL_VER}\`
+** Build number:** \`${BUILDKITE_BUILD_NUMBER}\`
+
+## Deployment Targets
+
+The application can be deployed to the following targets:
+$(for host in "${DEPLOY_HOST_ARRAY[@]}"; do
+    echo "- \`${host}\`"
+done)
+✅ **Build completed successfully**
+EOF
+
+printf '%b\n' "$(cat build-success.md" | buildkite-agent annotate --style "success" --context "built-artifact"
